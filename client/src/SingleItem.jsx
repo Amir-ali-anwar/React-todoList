3876271562848f7084bf74177c81e2ab1135ccf3
Amir Ali Anwar <amiralianwar611@gmail.com>
2023-11-10 15:45:36 +0500

    delete task successfully

--- client/src/SingleItem.jsx
+++ client/src/SingleItem.jsx
@@ -11,6 +11,15 @@ const SingleItem = ({ item }) => {
       queryClient.invalidateQueries({queryKey:["tasks"]})
     }
   })
+
+  const {mutate:deleteTask,isLoading}= useMutation({
+    mutationFn:({taskId,isDone})=>{
+      return customFetch.delete(`/${taskId}`)
+    },
+    onSuccess:()=>{
+      queryClient.invalidateQueries({queryKey:["tasks"]})
+    }
+  })
   return (
     <div className='single-item'>
       <input
@@ -29,7 +38,7 @@ const SingleItem = ({ item }) => {
       <button
         className='btn remove-btn'
         type='button'
-        onClick={() => console.log('delete task')}
+        onClick={() => deleteTask({taskId:item.id})}
       >
         delete
       </button>
